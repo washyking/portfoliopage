36804050638e5ad1693276335ec762512d01808f
washyking <maiblefive201@gmail.com>
2024-05-09 20:53:32 +0800

    Initial commit

---
+++ Alvins_page/src/Page.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="This is ISADNIQWNDJQW DQWDIQWDIQ WDQWNDNQWIDNIQWD QWIDNIQWNDIQWND" />
+
+      
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
---
+++ Alvins_page/src/components/Home.vue
@@ -0,0 +1,12 @@
+
+
+
+<template>
+    <div class="greetings">
+      <h1 class="green"></h1>
+      <h3>
+        IM CREAITN GA RANDOM PAGE
+        
+      </h3>
+    </div>
+</template>
